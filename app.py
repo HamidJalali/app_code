ddf3caa9182255fd238d6bfba56fbb25aae4d1bd
Hamid Jalali <36888678+HamidJalali@users.noreply.github.com>
2026-09-24 22:53:42 +0200

    Update greeting message in home endpoint

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.get("/")
 def home():
     return jsonify({
-        "message": "Hello from the Python web app! nice addition at 22:49!",
+        "message": "Hello from the Python web app! nice addition at 22:53!",
         "status": "running"
     })
 
