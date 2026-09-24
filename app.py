dc7f0c3acdf453c3539252ee8c8233ab7ccb198a
Hamid Jalali <36888678+HamidJalali@users.noreply.github.com>
2026-09-24 17:03:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.get("/")
 def home():
     return jsonify({
-        "message": "Hello from the Python web app!",
+        "message": "Hello from the Python web app! nice addition!",
         "status": "running"
     })
 
